50fdd5c1b1067ce2a2baf76297336fb0423e11e6
suraj810-cell <surajshrivas8109@gmail.com>
2024-09-28 19:12:23 +0530

    Add files via upload

---
+++ day 1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a;
+    cin >> a;
+    if(a>=12)
+    {
+        cout <<"yes";
+    }
+    else
+    {
+        cout << "no";
+    }
+
+}
